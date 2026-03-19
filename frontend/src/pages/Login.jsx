cbe3dfe92e0a154308009435689c9ab4328e3455
Jouni <83824082+joedelord@users.noreply.github.com>
2026-03-19 15:32:35 +0200

    feat: enhance login UI with form handling and styling

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,97 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../api";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+
 const Login = () => {
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+
+    try {
+      const res = await api.post(route, { email, password });
+      if (method === "login") {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+        navigate("/");
+      } else {
+        navigate("/login");
+      }
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div>
-      <h1>Login</h1>
+      <h1 className="text-2xl text-center m-10">Login</h1>
+      <div className="w-150 border-black border-2 p-5 rounded-md m-auto">
+        <form onSubmit={handleSubmit} class="max-w-sm mx-auto">
+          <div class="mb-5">
+            <label
+              for="email"
+              class="block mb-2.5 text-sm font-medium text-heading"
+            >
+              Sähköposti
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={mail}
+              onChange={(e) => setMail(e.target.value)}
+              class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+              placeholder="name@flowbite.com"
+              required
+            />
+          </div>
+          <div class="mb-5">
+            <label
+              for="password"
+              class="block mb-2.5 text-sm font-medium text-heading"
+            >
+              Salasana
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+              placeholder="••••••••"
+              required
+            />
+          </div>
+          <label for="remember" class="flex items-center mb-5">
+            <input
+              id="remember"
+              type="checkbox"
+              value=""
+              class="w-4 h-4 border border-default-medium rounded-xs bg-neutral-secondary-medium focus:ring-2 focus:ring-brand-soft"
+              required
+            />
+            <p class="ms-2 text-sm font-medium text-heading select-none">
+              I agree with the{" "}
+              <a href="#" class="text-fg-brand hover:underline">
+                terms and conditions
+              </a>
+              .
+            </p>
+          </label>
+          {loading}
+          <button
+            type="submit"
+            class="text-white bg-black box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none"
+          >
+            Kirjaudu
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
